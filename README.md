9c35e687b2d5560e47ca86aa385d176d61f52ccd
SARomanchuk <SARomanchuk@sberbank.ru>
2023-09-17 16:57:30 +0200

    start project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # drf_multy_serializers
+
+Components for creating ViewSets with support for serializers depending on the action and HTTP method.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+with Path("README.md").open("r") as fh:
+    long_description = fh.read()
+
+description = "Components for creating ViewSets with support for serializers depending on the action and HTTP method."
+install_requires = ("djangorestframework>=3.12.4",)
+
+setup(
+    name="drf_multy_serializers",
+    version="1.0.0",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GefMar/drf_multy_serializers",
+    author="Sergei (Gefest) Romanchuk",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8+",
+    ],
+)
